1abb614307353c489d4cabdb92c1ca726264cdf9
toothacher17 <toothacher17@gmail.com>
2014-12-11 17:06:24 +0800

    update 1211

--- src/SchoolSearch/startup/BuildStartup.java
+++ src/SchoolSearch/startup/BuildStartup.java
@@ -7,18 +7,21 @@ import SchoolSearch.services.suggestion.TrieHandle;
 import SchoolSearch.services.utils.T5RegistryHelper;
 
 public class BuildStartup {
+//	the class was used to build start up!
+	
 	public static void main(String[] args) {
 		TrieHandle trie = T5RegistryHelper.getService(TrieHandle.class);
 		trie.rebuild();
 		System.out.println("[trie rebuild finished]");
 		
-//		LuceneIndexBuilder lib = new LuceneIndexBuilder();
-//		try {
-//			lib.makeLuceneIndex();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		System.out.println("[lucene index rebuild finished]");
+		LuceneIndexBuilder lib = new LuceneIndexBuilder();
+		try {
+			lib.makeLuceneIndex();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("[lucene index rebuild finished]");
+		
 		T5RegistryHelper.shutdown();
 	}
 }
